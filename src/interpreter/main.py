35c5cbfae43477f4f404787e919abd49a1631519
dangduomg <jack98765.2007@gmail.com>
2025-01-04 22:12:33 +0700

    refactor ASTInterpreter to use only local environment for variable management

--- src/interpreter/main.py
+++ src/interpreter/main.py
@@ -28,14 +28,13 @@ class ASTInterpreter(ASTVisitor):
 
     # pylint: disable=too-few-public-methods
 
-    globals: Env
     locals: Env | None = None
 
     calls: list[Call]
 
     def __init__(self):
         self.calls = []
-        self.globals = Env(self)
+        self.locals = Env(self)
 
     def visit(self, node: nodes._Expr) -> Result:
         return self._visit(node)
@@ -51,25 +50,10 @@ class ASTInterpreter(ASTVisitor):
                 callee = self._visit(callee)
                 return callee.call([arg_visited], self, meta)
             case nodes.Var(meta=meta, name=name):
-                return self._get_var(name, meta)
+                return self.locals.get_var(name, meta)
             case nodes.Lambda(form_arg=form_arg, body=body):
                 env = None if self.locals is None else self.locals.copy()
                 return bl_types.BLFunction(form_arg, body, env, str(node))
         return BLError(cast_to_instance(
             NotImplementedException.new([], self, node.meta)
         ), node.meta)
-
-    def _get_var(self, name: str, meta: Meta) -> ExpressionResult:
-        """Get a variable either from locals or globals"""
-        if self.locals is not None:
-            res = self.locals.get_var(name, meta)
-            if isinstance(res, Value):
-                return res
-        return self.globals.get_var(name, meta)
-
-    def _new_var(self, name: str, value: Value) -> None:
-        """Assign a new variable either in locals or globals"""
-        if self.locals is not None:
-            self.locals.new_var(name, value)
-            return
-        self.globals.new_var(name, value)
