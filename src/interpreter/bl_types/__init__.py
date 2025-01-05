b1698255487c99639685bcb2d454a8838c37d6b7
dangduomg <jack98765.2007@gmail.com>
2025-01-05 18:47:14 +0700

    refactor: remove Result

--- src/interpreter/bl_types/__init__.py
+++ src/interpreter/bl_types/__init__.py
@@ -5,8 +5,8 @@
 # ruff: noqa: F401
 # flake8: noqa: F401
 from .essentials import (
-    Result, ExpressionResult, Value, BLError, String, Bool, BOOLS,
-    Null, NULL, PythonFunction, BLFunction, Call, Class, Instance,
-    ObjectClass, ExceptionClass, NotImplementedException,
-    IncorrectTypeException, VarNotFoundException, Env, cast_to_instance,
+    ExpressionResult, Value, BLError, String, Bool, BOOLS, Null, NULL, Call,
+    PythonFunction, BLFunction, Class, Instance, ObjectClass, ExceptionClass,
+    NotImplementedException, IncorrectTypeException, VarNotFoundException,
+    Env, cast_to_instance,
 )
